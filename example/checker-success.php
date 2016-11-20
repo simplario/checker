91a92e6fc71fbb19d577682746d6016f8c680fc8
vlad.groznov <vlad.groznov@gmail.com>
2016-11-20 21:05:45 +0200

    scrutinizer

--- example/checker-success.php
+++ example/checker-success.php
@@ -2,6 +2,7 @@
 
 return [
     'tasks' => [
-        ['checker' => 'filesystem', 'path' => __FILE__, 'exists' => true, 'readable' => true]
+        ['checker' => 'filesystem', 'path' => __FILE__, 'exists' => true, 'readable' => true],
+        ['checker' => Simplario\Checker\Checker\Filesystem::class, 'path' => __FILE__, 'exists' => true, 'readable' => true],
     ],
 ];
